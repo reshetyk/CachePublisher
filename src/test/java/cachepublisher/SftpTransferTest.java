359dc67db0bd1131ed67c7e57d27c48d23a20d86
Alexey <reshetyk@gmail.com>
2016-01-18 01:31:22 +0100

    fix

--- src/test/java/cachepublisher/SftpTransferTest.java
+++ src/test/java/cachepublisher/SftpTransferTest.java
@@ -2,17 +2,20 @@ package cachepublisher;
 
 import org.apache.sshd.SshServer;
 import org.apache.sshd.common.NamedFactory;
-import org.apache.sshd.common.file.virtualfs.VirtualFileSystemFactory;
+import org.apache.sshd.common.file.nativefs.NativeFileSystemFactory;
 import org.apache.sshd.server.Command;
+import org.apache.sshd.server.PublickeyAuthenticator;
 import org.apache.sshd.server.UserAuth;
 import org.apache.sshd.server.auth.UserAuthPublicKey;
 import org.apache.sshd.server.command.ScpCommandFactory;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.session.ServerSession;
 import org.apache.sshd.server.sftp.SftpSubsystem;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -40,25 +43,32 @@ public class SftpTransferTest {
     }
 
     private SshServer createSshServer( int port, String homeDir, String hostKeyPath ) {
+        System.setProperty("user.dir",homeDir);
         SshServer server = SshServer.setUpDefaultServer();
         server.setHost("localhost");
         server.setPort(port);
-        server.setFileSystemFactory( new VirtualFileSystemFactory( homeDir ) );
-        server.setSubsystemFactories( Collections.<NamedFactory<Command>>singletonList(new SftpSubsystem.Factory()) );
-        server.setCommandFactory( new ScpCommandFactory() );
-        server.setKeyPairProvider( new SimpleGeneratorHostKeyProvider( hostKeyPath ) );
-        server.setUserAuthFactories(Collections.<NamedFactory<UserAuth>>singletonList(new UserAuthPublicKey.Factory()) );
-//        server.setPasswordAuthenticator( new );
+//        server.setFileSystemFactory(new VirtualFileSystemFactory(homeDir));
+        server.setFileSystemFactory(new NativeFileSystemFactory());
+
+                server.setSubsystemFactories(Collections.<NamedFactory<Command>>singletonList(new SftpSubsystem.Factory()));
+        server.setCommandFactory(new ScpCommandFactory());
+        server.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(hostKeyPath));
+        server.setUserAuthFactories(Collections.<NamedFactory<UserAuth>>singletonList(new UserAuthPublicKey.Factory()));
+        server.setPublickeyAuthenticator(new PublickeyAuthenticator() {
+            public boolean authenticate(String username, PublicKey key, ServerSession session) {
+                return true;
+            }
+        });//        server.setPasswordAuthenticator( new );
         return server;
     }
 
     @Test
     public void testDoTransfer() throws Exception {
-        SshServer sshServer = createSshServer(28999, "c:/temp/home", "c:/temp/pair/hostkey.ser");
+        SshServer sshServer = createSshServer(28999, "C:/temp/home", "C:/temp/pair/hostkey.ser");
         sshServer.start();
 //        createAndStartSshServer(28999);
-        SftpTransfer sftpTransfer = new SftpTransfer("localhost", "user", 28999, "", "c:/temp");
-        sftpTransfer.doTransfer(new File("C:\\tmp\\2.txt"));
+        SftpTransfer sftpTransfer = new SftpTransfer("localhost", "user", 28999, "", "ok");
+        sftpTransfer.doTransfer(new File("C:/temp/home/to send/2.txt"));
         sshServer.stop();
     }
 }
